75a7bf1418e5d492eb2bf5694250b6814f40e8d5
Raghu K <raghuk.vit@gmail.com>
2025-12-05 07:29:03 +0530

    Roboshop project

--- network/main.tf
+++ network/main.tf
@@ -7,3 +7,15 @@ resource "aws_security_group" "main" {
   }
 }
 
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.main.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.main.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
